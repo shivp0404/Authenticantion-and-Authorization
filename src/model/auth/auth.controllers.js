092849695ee4d3f2caab6a9feacdf34a1d5581c7
shivansh Patel <152201750+shivp0404@users.noreply.github.com>
2025-12-27 19:12:23 +0530

    refactor(auth): integrate centralized error handling

--- src/model/auth/auth.controllers.js
+++ src/model/auth/auth.controllers.js
@@ -1,10 +1,15 @@
 const AuthServices = require('./auth.services')
 
 const AuthControllers = {
-    register: async(req,res)=>{
+    register: async(req,res,next)=>{
+        try{
          const data = req.body;
          const user = await AuthServices.registration(data);
          res.status(201).json({'message':'User Created','data':user})
+        }
+        catch(err){
+            next(err)
+        }
     }
 }
 
