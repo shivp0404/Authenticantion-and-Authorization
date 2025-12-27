6e0c5f8a3e7b0410aca2dd2bf85b108a7ef393e5
shivansh Patel <152201750+shivp0404@users.noreply.github.com>
2025-12-27 21:11:06 +0530

    chore:implement a logger and middleware.

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express();
+const HandleError = require('./src/middleware/errorHandler')
+const logger = require('./src/middleware/logger')
 
 app.use(express.json())
+app.use(logger)
+
+const AuthRoutes = require('./src/routes/AuthRoutes')
+
+app.use('/auth',AuthRoutes)
+
+app.use(HandleError)
 
 
 
