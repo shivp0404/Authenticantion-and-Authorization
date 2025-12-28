245f84512a7bc1744673ac7798808da34b3473f2
shivansh Patel <152201750+shivp0404@users.noreply.github.com>
2025-12-28 15:45:50 +0530

    feat(auth): add auth routes and wire them into app.

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express();
+const cookieParser = require('cookie-parser')
+
 const HandleError = require('./src/middleware/errorHandler')
 const logger = require('./src/middleware/logger')
 
 app.use(express.json())
+app.use(cookieParser())
 app.use(logger)
 
 const AuthRoutes = require('./src/routes/AuthRoutes')
