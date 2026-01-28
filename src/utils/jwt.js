473f5e6fd6d79a55572105dd9bdd96ef1f3f9638
shivansh Patel <152201750+shivp0404@users.noreply.github.com>
2026-01-28 22:56:43 +0530

    chore(utils):add a GenerateResetPasswordToken & decodeResetPasswordToken

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -18,5 +18,14 @@ const VerifyUser = (token)=>{
 const decodeRefreshToken = (token)=>{
    return jwt.verify(token,process.env.RefreshTokenSecret)
 }
-module.exports = {GenerateAccessToken,GenerateRefreshToken,VerifyUser,decodeRefreshToken}
+const GenerateResetPasswordToken = async (payload) => {
+  return jwt.sign(payload, process.env.RESET_PASSWORD_SECRET, {
+    expiresIn: "10m", // ✅ small expire time
+  });
+};
+
+const decodeResetPasswordToken = async (token) => {
+  return jwt.verify(token, process.env.RESET_PASSWORD_SECRET);
+};
+module.exports = {GenerateResetPasswordToken,decodeResetPasswordToken,GenerateAccessToken,GenerateRefreshToken,VerifyUser,decodeRefreshToken}
 
