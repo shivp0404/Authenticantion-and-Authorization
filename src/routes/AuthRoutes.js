5590c6bafce477a64925587470f41650405c1163
shivansh Patel <152201750+shivp0404@users.noreply.github.com>
2026-01-29 10:58:26 +0530

    feat(auth):add a controller and router for reset-Password

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -7,4 +7,5 @@ router.post('/login',AuthControllers.login)
 router.post('/logout',AuthControllers.logout)
 router.post('/refresh',AuthControllers.refresh)
 router.post('/forgotPassword',AuthControllers.forgotPassword)
+router.post('/reset-Password/:token',AuthControllers.resetPassword)
 module.exports = router;
