2fd6c660b1e0a088822edc238ca21248e3c70c0e
shivansh Patel <152201750+shivp0404@users.noreply.github.com>
2026-03-30 15:03:33 +0530

    l

--- src/utils/redisClient.js
+++ src/utils/redisClient.js
@@ -3,9 +3,8 @@ const Redis = require("ioredis");
 
 const redisClient = new Redis({
   host: process.env.REDIS_HOST ,
-  port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379,
+  port:  parseInt(process.env.REDIS_PORT),
   password: process.env.REDIS_PASSWORD ,
-  tls:{},
    connectTimeout: 10000 
 });
 // events
